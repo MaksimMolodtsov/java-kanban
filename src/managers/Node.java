a8f555db5f3c78222060eed404ea79a4690e3ec0
Maksim Molodtsov <maksimmolodtsov@gmail.com>
2025-01-12 13:43:48 +0300

    Исправления по Codestyle

--- src/managers/Node.java
+++ src/managers/Node.java
@@ -1,12 +1,12 @@
 package managers;
 
-public class Node <T> {
+public class Node<T> {
 
     public T data;
     public Node<T> next;
     public Node<T> prev;
 
-    public Node (Node<T> prev, T data, Node<T> next){
+    public Node(Node<T> prev, T data, Node<T> next) {
         this.data = data;
         this.next = next;
         this.prev = prev;
