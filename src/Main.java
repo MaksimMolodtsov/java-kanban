e62469e7442dab7bfe1c50d8bd8253223dadd25e
Maksim Molodtsov <maksimmolodtsov@gmail.com>
2025-01-12 14:50:22 +0300

    Исправления по exit code 254

--- src/Main.java
+++ src/Main.java
@@ -1,68 +1,70 @@
 import managers.Managers;
+import managers.TaskManager;
 import statuses.Status;
 import tasks.Epic;
 import tasks.Subtask;
 import tasks.Task;
-import managers.TaskManager;
 
-public static void main(String[] ignoredArgs) {
+public class Main {
+        public static void main(String[] args) {
 
-    TaskManager manager = Managers.getDefault();
+                TaskManager manager = Managers.getDefault();
 
-        Task task1 = new Task("Задача №1", "Поспать", Status.IN_PROGRESS);
-        manager.addTask(task1);
-        Task task2 = new Task("Задача №2", "Еще поспать", Status.NEW);
-        manager.addTask(task2);
+                Task task1 = new Task("Задача №1", "Поспать", Status.IN_PROGRESS);
+                manager.addTask(task1);
+                Task task2 = new Task("Задача №2", "Еще поспать", Status.NEW);
+                manager.addTask(task2);
 
-        Epic epic1 = new Epic("Эпик №1", "Съездить в отпуск");
-        manager.addEpic(epic1);
-        Subtask subtask11 = new Subtask("Эпик №1, Подзадача №1", "Найти тур", Status.DONE, epic1.getId());
-        manager.addSubtask(subtask11);
-        Subtask subtask12 = new Subtask("Эпик №1, Подзадача №2", "Оплата", Status.IN_PROGRESS, epic1.getId());
-        manager.addSubtask(subtask12);
+                Epic epic1 = new Epic("Эпик №1", "Съездить в отпуск");
+                manager.addEpic(epic1);
+                Subtask subtask11 = new Subtask("Эпик №1, Подзадача №1", "Найти тур", Status.DONE, epic1.getId());
+                manager.addSubtask(subtask11);
+                Subtask subtask12 = new Subtask("Эпик №1, Подзадача №2", "Оплата", Status.IN_PROGRESS, epic1.getId());
+                manager.addSubtask(subtask12);
 
-        Epic epic2 = new Epic("Эпик №2", "Подумать о смысле жизни");
-        manager.addEpic(epic2);
-        Subtask subtask21 = new Subtask("Эпик №2, Подзадача №1", "Думать", Status.IN_PROGRESS, epic2.getId());
-        manager.addSubtask(subtask21);
+                Epic epic2 = new Epic("Эпик №2", "Подумать о смысле жизни");
+                manager.addEpic(epic2);
+                Subtask subtask21 = new Subtask("Эпик №2, Подзадача №1", "Думать", Status.IN_PROGRESS, epic2.getId());
+                manager.addSubtask(subtask21);
 
-        System.out.println(manager.getTasks());
-        System.out.println(manager.getEpics());
-        System.out.println(manager.getSubtasks());
+                System.out.println(manager.getTasks());
+                System.out.println(manager.getEpics());
+                System.out.println(manager.getSubtasks());
 
-        task1.setStatus(Status.DONE);
-        task2.setStatus(Status.IN_PROGRESS);
-        subtask11.setStatus(Status.IN_PROGRESS);
-        subtask12.setStatus(Status.DONE);
-        subtask21.setStatus(Status.DONE);
+                task1.setStatus(Status.DONE);
+                task2.setStatus(Status.IN_PROGRESS);
+                subtask11.setStatus(Status.IN_PROGRESS);
+                subtask12.setStatus(Status.DONE);
+                subtask21.setStatus(Status.DONE);
 
-        manager.updateTask(task1);
-        manager.updateTask(task2);
-        manager.updateSubtask(subtask11);
-        manager.updateSubtask(subtask12);
-        manager.updateSubtask(subtask21);
+                manager.updateTask(task1);
+                manager.updateTask(task2);
+                manager.updateSubtask(subtask11);
+                manager.updateSubtask(subtask12);
+                manager.updateSubtask(subtask21);
 
-        System.out.println(manager.getTasks());
-        System.out.println(manager.getEpics());
-        System.out.println(manager.getSubtasks());
-        System.out.println(manager.getSubtasksOfEpic(epic1.getId()));
+                System.out.println(manager.getTasks());
+                System.out.println(manager.getEpics());
+                System.out.println(manager.getSubtasks());
+                System.out.println(manager.getSubtasksOfEpic(epic1.getId()));
 
-        manager.deleteTaskById(task1.getId());
-        manager.deleteEpicById(epic2.getId());
-        manager.deleteSubtaskById(subtask12.getId());
+                manager.deleteTaskById(task1.getId());
+                manager.deleteEpicById(epic2.getId());
+                manager.deleteSubtaskById(subtask12.getId());
 
-        System.out.println(manager.getTasks());
-        System.out.println(manager.getEpics());
-        System.out.println(manager.getSubtasks());
+                System.out.println(manager.getTasks());
+                System.out.println(manager.getEpics());
+                System.out.println(manager.getSubtasks());
 
-        System.out.println(manager.getTaskById(2));
-        System.out.println(manager.getEpicById(3));
-        System.out.println(manager.getSubtaskById(4));
+                System.out.println(manager.getTaskById(2));
+                System.out.println(manager.getEpicById(3));
+                System.out.println(manager.getSubtaskById(4));
 
-        System.out.println(manager.getHistory());
+                System.out.println(manager.getHistory());
 
-        System.out.println(manager.getTaskById(2));
-        System.out.println(manager.getEpicById(3));
+                System.out.println(manager.getTaskById(2));
+                System.out.println(manager.getEpicById(3));
 
-        System.out.println(manager.getHistory());
-    }
+                System.out.println(manager.getHistory());
+        }
+}
